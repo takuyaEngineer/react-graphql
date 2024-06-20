52daee09cc03b645e640892fe38148acd2b680f3
takuyaEngineer <engineer.memorandum@gmail.com>
2024-06-20 13:53:31 +0900

    graphqlからデータ取得できた

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const res = gql`
+  query {
+    users {
+      name
+      id
+      email
+      name
+      _count {
+        posts
+      }
+      posts {
+        content
+        title
+        published
+        authorId
+      }
+    }
+  }
+`;
+
+type post = {
+  authorId: number;
+  content: string;
+  published: boolean;
+  title: string;
+};
+
+type user = {
+  email: string;
+  name: string;
+  id: number;
+  posts: post[];
+};
+
+const TodoPrivateListQuery = () => {
+  const { loading, error, data } = useQuery(res);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    console.error(error);
+    return <div>Error!</div>;
+  }
+  const users: user[] = data;
+  return users;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const users = TodoPrivateListQuery();
+  console.log(users);
+
+  return <div>hello world</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "http://localhost:3000/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
